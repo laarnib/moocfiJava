eb55dd7cc8b29651c1809e1c8c4d7575850a6afc
Laarni Bernardo <laarnijb@gmail.com>
2021-07-04 15:25:32 -0400

    add completed solutions to exercises 21 - 33 of Java Programming I Part 2

---
+++ mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        divisibleByThreeInRange(3, 10);
+    }
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
