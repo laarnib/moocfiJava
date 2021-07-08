8c6c225a424ac83b304f04efce91c81a91602745
Laarni Bernardo <laarnijb@gmail.com>
2021-07-08 12:37:59 -0400

    add solutions to exercises 1 and 2 of Part 4 Java Programming I

---
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account account = new Account("Jazzy Jazz", 100.0);
+                
+        account.deposit(20.0);
+        System.out.println(account);
+    }
+}
