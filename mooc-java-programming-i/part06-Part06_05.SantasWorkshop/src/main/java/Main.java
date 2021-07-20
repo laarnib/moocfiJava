d6155ee22e5e6c649b5906ac7115854d4035e1b1
Laarni Bernardo <laarnijb@gmail.com>
2021-07-20 10:52:15 -0400

    add code in Main to test the new classes

--- mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_05.SantasWorkshop/src/main/java/Main.java
@@ -2,9 +2,17 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
         // use this main method to try out your classes!
-
+        Gift toy = new Gift("Truck", 2);
+        Gift book = new Gift("Book", 1);
+        
+        System.out.println("Gift name: " + toy.getName());
+        System.out.println("Gift weight: " + toy.getWeight());
+        System.out.println("Gift: " + toy);
+        
+        Package gifts = new Package();
+        gifts.addGift(toy);
+        gifts.addGift(book);
+        System.out.println(gifts.totalWeight());
     }
 }
