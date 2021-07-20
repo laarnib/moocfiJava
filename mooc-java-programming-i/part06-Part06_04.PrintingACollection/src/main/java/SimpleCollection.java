c15317b1f68e9f9937746d8075078716c98c955f
Laarni Bernardo <laarnijb@gmail.com>
2021-07-20 10:32:19 -0400

    override toString method to print the collection

--- mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ mooc-java-programming-i/part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -19,4 +19,25 @@ public class SimpleCollection {
         return this.elements;
     }
     
+    @Override
+    public String toString() {
+        String listItems = "";
+        int elementsCount = this.elements.size();
+        
+        if (elementsCount == 0) {
+            return "The collection " + this.name + " is empty.";
+        } 
+        
+        for (String element : this.elements) {
+            listItems = listItems + "\n" + element;
+        }
+        
+        if (elementsCount == 1) {
+            return "The collection " + this.name + " has " + elementsCount + 
+                    " element:" + listItems;
+        }
+        
+        return "The collection " + this.name + " has " + elementsCount + 
+                " elements:" + listItems;
+    }    
 }
