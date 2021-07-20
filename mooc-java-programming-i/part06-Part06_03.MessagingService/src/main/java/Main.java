bb8aac02dac9a530af5c721386ec2c5a02f68485
Laarni Bernardo <laarnijb@gmail.com>
2021-07-20 10:08:43 -0400

    add code to test the MessagingService class

--- mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
+        Message newMessage = new Message("Torch", "Can't go tonight");
+        MessagingService messages = new MessagingService();
+        
+        messages.add(newMessage);
+        System.out.println(messages.getMessages());        
     }
 }
