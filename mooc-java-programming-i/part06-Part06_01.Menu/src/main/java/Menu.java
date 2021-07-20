e5ea28a7a59b45ecb33fed04a40f23fc206207bd
Laarni Bernardo <laarnijb@gmail.com>
2021-07-20 09:15:01 -0400

    add addMeal, printMeals, and clearMenu methods

--- mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
+++ mooc-java-programming-i/part06-Part06_01.Menu/src/main/java/Menu.java
@@ -10,4 +10,21 @@ public class Menu {
     }
 
     // implement the required methods here
+    // Methods added by LB
+    public void addMeal(String meal) {
+        if (!this.meals.contains(meal)) {
+            this.meals.add(meal);
+        }
+    }
+    
+    public void printMeals() {
+        for (String item : meals) {
+            System.out.println(item);
+        }
+    }
+    
+    public void clearMenu() {
+        meals.clear();
+    }
+    
 }
