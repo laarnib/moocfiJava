037bd44a4fdc628c8de99126007441d7b201469c
Laarni Bernardo <laarnijb@gmail.com>
2021-07-26 15:24:45 -0400

    - add new ArrayList type variable to store the exam points - added method to calculate the average of the exam points - added method to calculate the average of the grades

--- mooc-java-programming-i/part06-Part06_11.Averages/src/main/java/GradeRegister.java
+++ mooc-java-programming-i/part06-Part06_11.Averages/src/main/java/GradeRegister.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 public class GradeRegister {
 
     private ArrayList<Integer> grades;
+    private ArrayList<Integer> points; // Added by LB. Stores the exam points
 
     public GradeRegister() {
         this.grades = new ArrayList<>();
+        this.points = new ArrayList<>();
     }
 
     public void addGradeBasedOnPoints(int points) {
         this.grades.add(pointsToGrade(points));
+        
+        // Added by LB
+        // Add exam points to points ArrayList
+        this.points.add(points);
     }
 
     public int numberOfGrades(int grade) {
@@ -43,4 +49,38 @@ public class GradeRegister {
 
         return grade;
     }
+    
+    // Added by LB
+    public double averageOfGrades() {
+        int numberOfGrades = 0, sum = 0;
+        
+        if (this.grades.isEmpty()) {
+            return -1;
+        }
+        
+        numberOfGrades = this.grades.size();
+        for (Integer grade : grades) {
+            sum += grade;
+        }
+        
+        return (double)sum / numberOfGrades;
+    }
+    
+    // Added by LB
+    // Calculates the average of the exam points
+    public double averageOfPoints() {
+        int numberOfPoints = 0, sum = 0;
+        
+        if (this.points.isEmpty()) {
+            return -1;
+        }
+        
+        numberOfPoints = this.points.size();
+        for (Integer point : points) {
+            sum += point;
+        }
+        
+        return (double)sum / numberOfPoints;
+    }
+           
 }
